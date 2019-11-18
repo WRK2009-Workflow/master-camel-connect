cc5637079cb54a4e25687d397e1e30a9bc1049f7
Kevin Alwell <alwell-kevin@github.com>
2019-11-18 12:16:41 -0500

    Update update-issues.js

--- .github/actions/update-issues-action/main/automation/update-issues.js
+++ .github/actions/update-issues-action/main/automation/update-issues.js
@@ -8,16 +8,17 @@
 
 // // GETS INPUTS FROM ACTION
 // const body = core.getInput('body');
+// const GITHUB_TOKEN = core.getInput('GITHUB_TOKEN');
 
 // // When invoked, create an issue in my repo. :)
 // const updateRepo = async () => {
 //   // @actions/github is a nice utility to help us make calls to the GitHub API (https://github.com/actions/toolkit/tree/master/packages/github)
 //   // GITHUB_TOKEN is an automatically generated secret scoped to the repo that the workflows run in (https://help.github.com/en/actions/automating-your-workflow-with-github-actions/authenticating-with-the-github_token#about-the-github_token-secret)
-//   const github = new GitHub(process.env.GITHUB_TOKEN); 
+//   const github = new GitHub(GITHUB_TOKEN); 
 //   // SIMPLE REQUEST TO GITHUB API TO CREATE AN ISSUE IN THE ORG/REPO
 //   await github.issues.create({
 //       ...context.repo,
-//       title: 'Test issue',
+//       title: 'Actions Generated Issue',
 //       body
 //   });
 
